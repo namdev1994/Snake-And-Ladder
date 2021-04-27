755e82f679fd90e189bffeaefa7f5e4d2ea297d4
namdev1994 <n.lavhare1994@gmail.com>
2021-04-27 19:33:42 +0530

    Added: Dice Random Number Between 1 to 6

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -1,9 +1,13 @@
 package snakeAndLadder;
+import java.util.*;
 
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int start_Position=0;
+		Random rand =new Random();
+		int dice=rand.nextInt(6)+1;
+		System.out.println(dice);
 
 	}
 
